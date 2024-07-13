6b06f04f6d6174f7fb79212f028fed19d79cb149
Ekaterina Prudnikova <k_pr_89@mail.ru>
2024-07-13 20:14:38 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BonusService service = new BonusService();
+        long bonus = service.calculate(10_000, true);
+        System.out.println("Сумма бонуса: " + bonus);
+    }
+}
